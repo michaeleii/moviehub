76220a543e7d46083617b95d317acfade1f2aacb
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-06-13 21:52:21 -0700

    Remove Star Rating

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import StarRating from "./components/StarRating.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
